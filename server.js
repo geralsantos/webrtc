323745dca4f12f48b103f3d7320e09c8028582e7
geralsantos <geral_366@hotmail.com>
2020-01-24 11:13:59 -0500

    update

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    
+socket.on('initiate', () => {
+    io.emit('initiate');
+});
+socket.on('offer', (data) => {
+    socket.broadcast.emit('offer', data);
+});
+});
+app.use(express.static('public'));
+
+http.listen(3000, () => console.log('Example app listening on port 3000!'))
