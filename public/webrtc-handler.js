05918af28d50649cb52c736fabed56242787fc66
geralsantos <geral_366@hotmail.com>
2020-02-02 16:13:29 -0500

    p

--- public/webrtc-handler.js
+++ public/webrtc-handler.js
@@ -1,22 +1,45 @@
 var socket = io.connect();
-
+var Peer = require('simple-peer')
 var initiateBtn = document.getElementById('initiateBtn');
+var lookBtn = document.getElementById('lookBtn');
+var video = document.querySelector('video');
+
 var initiator = false;
 
 initiateBtn.onclick = (e) => {
     initiator = true;
+    geral();
     socket.emit('initiate');
 }
-async  function geral(){
+lookBtn.onclick = (e) => {
+    initiator = false;
+    geral();
+}/*
+socket.on('consult_initiate', (response) => {
+    alert(initiator)
+    if (!initiator) {
+        geral();
+    }
+    
+});*/
+ function geral(){
     try {
-        await navigator.mediaDevices.getUserMedia({
-            video: {
+        var constraints = {
+            audio: true, video:  {
                 mediaSource: "screen",
                 width: { max: '1920' },
                 height: { max: '1080' },
                 frameRate: { max: '10' }
             }
-        }).then(gotMedia)
+          };
+          if (initiator) {
+            navigator.getUserMedia_ = navigator.mediaDevices.getDisplayMedia(constraints)
+      
+          }else{
+              console.log("usuarios")
+            navigator.getUserMedia_ = navigator.mediaDevices.getUserMedia({audio: true})
+          }
+          navigator.getUserMedia_.then((stream)=>gotMedia(stream))
         .catch(function(err){
             console.log(err)
         });
@@ -24,34 +47,58 @@ async  function geral(){
         console.log(error)
     }
 }
-geral();
-function gotMedia(stream) {
-    var video = document.querySelector('video');
 
+const stunServerConfig = {  
+    iceServers: [{  
+      url: "stun:stun01.sipphone.com",  
+      username: "gera",  
+      credential: "123"  
+    }]  
+  };
+function gotMedia(stream) {
+    var peer = null;
+    console.log("gotMedia initiator",initiator)
     if (initiator) {
-        var peer = new Peer({
-            initiator,
+        peer = new Peer({
+            initiator:initiator,
             stream,
-            config: stunServerConfig
+           // config: stunServerConfig
         });
     } else {
-        var peer = new Peer({
-            config: stunServerConfig
+        peer = new Peer({
+            trickle: false,
+            //config: stunServerConfig
         });
     }
+    if (!initiator) {
+        socket.emit('traer_token');
+        console.log('traer_token')
+
+        socket.on('mostrar_token', (data) => {
+            console.log('mostrar_token',data)
+            peer.signal(JSON.parse(data))
+        })
+    }
     peer.on('signal', function (data) {
+console.log("signal")
         socket.emit('offer', JSON.stringify(data));
     });
     socket.on('offer', (data) => {
+        console.log(data)
         peer.signal(JSON.parse(data))
     })
     peer.on('stream', function (stream) {  
         // got remote video stream, now let's show it in a video tag  
+        console.log("geral")
         var video = document.querySelector('video');  
-        video.srcObject = stream;  
+        if ('srcObject' in video) {
+            video.srcObject = stream
+          } else {
+            video.src = window.URL.createObjectURL(stream) // for older browsers
+          }
         video.play();  
       })
 
-    video.srcObject = stream;
-    video.play();
+   /* video.srcObject = stream;
+    video.play();*/
 }
--- server.js
+++ server.js
@@ -2,14 +2,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var initiator = false;
+var str = null;
 io.on('connection', (socket) => {
     console.log('user connected');
     
 socket.on('initiate', () => {
+    initiator = true;
     io.emit('initiate');
+    io.emit('consult_initiate',{initiator:initiator});
+});
+socket.on('consult_initiate', () => {
+    io.emit('consult_initiate',{initiator:initiator});
+});
+socket.on('traer_token', () => {
+    io.emit('mostrar_token',str);
 });
 socket.on('offer', (data) => {
+    str = data;
     socket.broadcast.emit('offer', data);
 });
 });
